cc4e31843871b96742e8c8b70a2b47d6ca3abf85
healingfields <dihratene@gmail.com>
2022-07-10 00:39:10 +0100

    using my custom hook

--- management-board/src/pages/Board/Board.js
+++ management-board/src/pages/Board/Board.js
@@ -1,38 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import useDataFetching from '../../hooks/useDataFetching';
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 
 function Board() {
+
+
     const lanes = [
-        { 'id': 1, name: 'To do' },
-        { 'id': 2, name: 'In Progress' },
-        { 'id': 3, name: 'Review' },
-        { 'id': 4, name: 'Done' }
-    ]
-    const [loading, setLoading] = useState(true);
-    const [tasks, setTasks] = useState([]);
-    const [error, setError] = useState('');
+        { id: 1, title: 'To Do' },
+        { id: 2, title: 'In Progress' },
+        { id: 3, title: 'Review' },
+        { id: 4, title: 'Done' },
+    ];
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await fetch(
-                    'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks'
-                )
-                const result = await data.json();
-
-                if (result) {
-                    setTasks(result);
-                    setLoading(false);
-                }
-            }
-            catch (e) {
-                setLoading(false);
-                setError(e.message);
-            }
-        }
-        fetchData();
-    }, [])
+    const [loading, error, data] = useDataFetching(
+        'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks'
+    )
     return (
         <div className='board-container'>
             {lanes.map((lane) => (
@@ -40,7 +22,7 @@ function Board() {
                     name={lane.name}
                     loading={loading}
                     error={error}
-                    tasks={tasks.filter((task) =>
+                    tasks={data.filter((task) =>
                         task.lane === lane.id)} />
             ))}
         </div>
