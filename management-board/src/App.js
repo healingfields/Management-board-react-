4f81074476e8a0ab6ac3f639975bfaa21e59499e
healingfields <dihratene@gmail.com>
2022-07-10 00:39:29 +0100

    created the backlog component

--- management-board/src/App.js
+++ management-board/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Backlog from './pages/Backlog/Backlog';
 import Board from './pages/Board/Board';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Board />
+      <Backlog />
     </div>
   );
 }
---
+++ management-board/src/pages/Backlog/Backlog.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Task from '../../components/Task/Task';
+import useDataFetching from '../../hooks/useDataFetching';
+import './Backlog.css'
+
+function Backlog() {
+    const [loading, error, tasks] = useDataFetching(
+        'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks'
+    )
+    return (
+        <div className='backlog-container'>
+            <h2 className='title'>Backlog</h2>
+            <div className='tasks-wrapper'>
+                {loading || error ? (
+                    <span>{error || 'loading...'}</span>
+                ) : (
+
+                    tasks.map((task) => (
+                        <Task
+                            key={task.id}
+                            title={task.title}
+                            body={task.body} />
+                    ))
+                )}
+
+            </div>
+        </div>
+    )
+}
+
+export default Backlog
